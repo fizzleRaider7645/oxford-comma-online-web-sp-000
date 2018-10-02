56cebe77ac5e21c9fd3c42431472d4379db478c1
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-02 14:52:35 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   return array.join("") if array.length == 1
   return array.join(" and ") if array.length == 2
   if array.length == 3
-    array[2] << "and"
+    and_added = array[2] << "and"
     
   end
 end
