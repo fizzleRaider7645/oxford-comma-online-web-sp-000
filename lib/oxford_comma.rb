966981eefd94050c2be478f4f0bfcae5bf622a74
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-02 14:52:26 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,7 @@
 def oxford_comma(array)
   return array.join("") if array.length == 1
   return array.join(" and ") if array.length == 2
-  if array[2] << "and"
+  if array.length == 3
+    array[2] << "and"
+  end
 end
