14bc374e32de5171697b9a414426348ef82bc0ff
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-02 15:01:06 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   return array.join("") if array.length == 1
   return array.join(" and ") if array.length == 2
   if array.length == 3
-    p front = array.take(2).join(", ")
+    p front = array.take(2).join(", ") << ", "
     back = array.drop(2).unshift('and').join(' ')
   
   end
